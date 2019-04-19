a8bab1ea9552b1b771eb280492ed842afbd2071c
Akutin Artem <akutin_artem@mail.ru>
2019-04-19 23:00:50 +0400

    Backend (#4)
    * add delete endpoint and refactor
    
    * add backend actions
    
    * add tags endpoints

--- backend/src/main/java/mobile/lab3/note/servicescontracts/TagServicable.java
+++ backend/src/main/java/mobile/lab3/note/servicescontracts/TagServicable.java
@@ -1,7 +1,11 @@
 package mobile.lab3.note.servicescontracts;
 
 import mobile.lab3.note.common.entity.Tags;
+import mobile.lab3.note.common.viewmodels.AddTagModel;
+
+import javax.validation.ValidationException;
 
 public interface TagServicable {
     Iterable<Tags> getAll();
+    boolean add(AddTagModel model) throws ValidationException;
 }
